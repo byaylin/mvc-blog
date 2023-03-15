5230fece97687975d772452edc9f98fe214e49b3
byaylin <aylinxmorales@gmail.com>
2023-03-14 22:15:32 -0700

    added config file

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const sequelize = require('./config/connection')
 app.get('/', (req, res) => {
     res.send("Hi welcome to my tech blog")
 })
-    app.listen(PORT, function(){
-        console.log('Listening on PORT ' + PORT)
-    })
+
+sequelize.sync({ force: true }).then(function(){
+app.listen(PORT, function(){
+        console.log('Listening on PORT ' + PORT)}
+    )
+})
